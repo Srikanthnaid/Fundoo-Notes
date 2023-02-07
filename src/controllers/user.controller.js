c16caa7c628f6b68a3153562678a83cd964f8603
Srikanthnaid <ksru044gmail.com>
2023-02-07 10:37:49 +0530

    Added

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,41 @@
+import HttpStatus from 'http-status-codes';
+import * as UserService from '../services/user.service';
+
+
+/**
+ * Controller to create a new user
+ * @param  {object} req - request object
+ * @param {object} res - response object
+ * @param {Function} next
+ */
+export const registerNewUser = async (req, res, next) => {
+  try {
+    const data = await UserService.newUserRegister(req.body);
+    res.status(HttpStatus.CREATED).json({
+      code: HttpStatus.CREATED,
+      data: data,
+      message: 'User register successfully'
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Controller to login a user
+ * @param  {object} req - request object
+ * @param {object} res - response object
+ * @param {Function} next
+ */
+ export const login = async (req, res, next) => {
+  try {
+    const data = await UserService.login(req.body);
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+      message: 'user login successfully'
+    });
+  } catch (error) {
+    next(error);
+  }
+};
