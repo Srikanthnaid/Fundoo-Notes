660eaff247b1aa6e1099e092d631950830338ad7
Srikanthnaid <ksru044gmail.com>
2023-02-08 10:58:42 +0530

    Added: Generate a jwt(JSON WEB TOKEN)

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,18 +1,27 @@
 import { error } from 'winston';
 import User from '../models/user.model';
+import bcrypt from 'bcrypt';
+//import jwt
+import jwt from 'jsonwebtoken';
 
 //register new user
-export const register = async (body) => {
-const data = await User.create(body);
-return data;
+export const newUserRegister = async (body) => {
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(body.password, salt);
+  body.password = hash;
+  const data = await User.create(body);
+  return data;
 };
 
 // user login
 export const login = async (body) => {
   const data = await User.findOne({ email: body.email });
   if (data != null) {
-    if (body.password == data.password) {
-      return data;
+    const result = await bcrypt.compare(body.password, data.password);
+    if (result) {
+      var token = jwt.sign({firstname:data.firstname,email:data.email,id:data.id},process.env.SECRET_KEY);
+      return token;
     }
     else {
       throw new Error("Invalid Password");
